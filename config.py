9854e96ebca27d70258333ae77424225b7b56209
ʑɑʑɓɦɑɪ <87512658+Zazbhai@users.noreply.github.com>
2022-10-21 17:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://lvxjlgee:8FLuaV-Mw9KNFvkZ3nCHEOgjwa8jnsQG@jelani.db.elephantsql.com/lvxjlgee" # IF U DONT KNOW HOW TO MAKE THEN GO IN ZG SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1AZWarzkBu0c-VnypWD55GSzim_d1e-UowzZ0MIz8y7fgMMyEWU3GWBja_tPEQ6s8db0yyivuP6jkgq1_Lh1U2FjLTJkm8VM4e_8ebFMD8kDik9WawCguC1RuDCAnbWjh8nldZEPpN3zNAlvyTLGujqS2NETcGkeGyYpDLMXhyii6FmbS1H9H15yYKP5A6q0KPaSeoD0F-d_MREO2yfRZ9Hkd1lF42DI0Crg6gazhZxFIsBXxECtlWp1M8GzE9py69JtDqw1rSZiNGc89_iQ9vSa_xW9HHghCmGhzBJ_xzLmPM4sjQ3n2gN1VOLS5HiBJzVD2__g1zLL4so-YrJxMlphvmcg-k1c="# IF U DONT KNOW HOW TO MAKE THEN GO IN ZG SUPPORT AND TYPE #session 
+    STRING_SESSION = "1AZWarzcBu4Me07tdbHzll173qNk7C8qzycI4Nr17gcXI9eZtSEIfu6Oh4gojdplF9ZYtdXpx3QsQlZvuobAo2qWOJdP7a8redAYa48wPawIpoZ9TJQNkOM-3THgJIV88G6QB-CDoYf4Mx35nCGbZZ-LiH1CQS59cuoRhQqhIf1K7Enuisjsu11K5SjXPMJxTP8LRTTtUVjndWwoxoMa7QkEFb2vmBLjNkjEEJ48mIFLPatB7_VQ17uYvsmb-gcTE-W_R5GzSJk5BiHg8WW49j_rXtPZTvOoB_LbdgtIYaAzaxaFFS_gwf1EmGEWDwu2B-UWd5duufLDc1pNnHFKxOQqnkRQwHO0="# IF U DONT KNOW HOW TO MAKE THEN GO IN ZG SUPPORT AND TYPE #session 
     # --------------------------------------------------
 
     TG_BOT_TOKEN = "5112208449:AAEwmbUrTq_BUumzKDY850_qS1RZvEqriaw"# create a new bot in @botfather and fill the following vales with bot token
