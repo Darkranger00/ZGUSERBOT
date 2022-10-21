061ab51f0a3503abef002b74b1d4ac3cdedd7ca9
ʑɑʑɓɦɑɪ <87512658+Zazbhai@users.noreply.github.com>
2022-10-21 15:25:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 4524654
+    API_HASH = "4cfbcd31ec888cc3f0709d8d20607ac8"
+    # the name to display in your alive message
+    ALIVE_NAME = "ʑɠ ცơɬ "
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "postgres://lvxjlgee:8FLuaV-Mw9KNFvkZ3nCHEOgjwa8jnsQG@jelani.db.elephantsql.com/lvxjlgee" # IF U DONT KNOW HOW TO MAKE THEN GO IN ZG SUPPORT AND TYPE #DB_URI 
+    # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
+    STRING_SESSION = "1AZWarzgBu37IZGMZ96sOVlNWquW8NdPD97hX2zKZPasUGA5anRqQFHhkSy9MuK0pY9Md4mqu0BkdU_5frve4mIHBJirb7zlpIhzeRXbbwjXcNiUTTw5gkooCftOm7syWkw_Zq0TRn80nBnDZemJl_qqrAmFDONpqvH8lEJb6NuB-IjmGg8g_cKnCsXdOuRwH5K4AL9IAugwdhMk3LQxU_-1PWNKSqt6dVqL_VIJrHXlMWYLNmyYxuR0SntIupiYL2ndReGhXgHhnsgjs5atv4CPp6018dnkIQTR16Rvd4936_aKY86WAgfjbwu_2meNiThHWWy_35qDMvO8L6B3f0nPoKQssPmA="# IF U DONT KNOW HOW TO MAKE THEN GO IN ZG SUPPORT AND TYPE #session 
+    # --------------------------------------------------
+
+    TG_BOT_TOKEN = "5112208449:AAEwmbUrTq_BUumzKDY850_qS1RZvEqriaw"# create a new bot in @botfather and fill the following vales with bot token
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+    PRIVATE_GROUP_BOT_API_ID = -1001704784226
+    # command handler
+    COMMAND_HAND_LER = "."
+    VCMODE = "false" #do it false if you don't want vc bot
+    VC_SESSION = "your assistant id session"
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
+    # External plugins repo
+    EXTERNAL_REPO = "https://github.com/Zazbhai/tha_plugins"
+    #don't edit this 
+    ZGABUSE = "False"#don't edit this
